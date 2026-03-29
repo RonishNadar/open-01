c9e16422fd0a4141f5b455067b748bd520fc2cdc
RonishNadar <nadar.ronish@email.com>
2026-03-29 18:32:33 -0400

    feat: Add open01_description ROS2 package with URDF model
    - Added open01.urdf defining robot structure with base_link, wheels, IMU, and TOF sensor links.
    - Migrated package from ROS1 catkin to ROS2 ament_cmake build system.
    - Updated CMakeLists.txt and package.xml for ROS2 compatibility.
    - Renamed all Robot_Assembly_1 references to open01_description.
    - Added joint_names_open01.yaml for joint configuration.
    - Updated .gitignore to exclude ROS2 build artifacts.

--- robot_ros2/ros2_ws/build/open01_bringup/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/open01/open-01/robot_ros2/ros2_ws/install/open01_serial_bridge/share/open01_serial_bridge/package.sh"
--- robot_ros2/ros2_ws/build/open01_bringup/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/open01/open-01/robot_ros2/ros2_ws/install/open01_bringup'
